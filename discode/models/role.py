9804b508f4a85f059ea9edd012ce68eaef78ce94
TheKungFuChuck <>
2022-03-28 07:15:52 +0000

    Add role tags

--- discode/models/role.py
+++ discode/models/role.py
@@ -1,14 +1,33 @@
+from __future__ import annotations
+
 __all__ = ("Role",)
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 from ..flags import Permissions
-from ..utils import UNDEFINED
+from ..utils import UNDEFINED, NamedTuple
 from .abc import Snowflake
 
+class RoleTags(NamedTuple):
+    bot_id: Optional[int]
+    integration_id: Optional[int]
+    premium_subscriber: bool
+    
+    def __init__(self, **data):
+        bot_id = int(data['bot_id']) if 'bot_id' in data else None
+        integration_id = int(data['integration_id']) if 'integration_id' in data else None
+        
+        super().__init(
+            "RoleTags",
+            **{
+                "bot_id": bot_id,
+                "integration_id": integration_id,
+                "premium_subscriber": data.pop("premium_subscriber", False)
+            }
+        )
 
 class Role(Snowflake):
-    r"""Represents a Discord role.__all__"""
+    r"""Represents a Discord role."""
 
     if TYPE_CHECKING:
         id: int
@@ -17,6 +36,7 @@ class Role(Snowflake):
         hoist: bool
         position: int
         permissions: Permissions
+        tags: RoleTags
 
     def __init__(self, connection, payload):
         self._connection = connection
@@ -26,6 +46,7 @@ class Role(Snowflake):
         self.hoist = payload.pop("hoist", False)
         self.position = payload.pop("position", None)
         self.permissions = Permissions(int(payload.pop("permissions", UNDEFINED)))
+        self.tags = RoleTags(**payload.pop("tags", {}))
 
     @property
     def mention(self) -> str:
